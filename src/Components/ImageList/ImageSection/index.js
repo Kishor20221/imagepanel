b593ee5331b589e0c4b7479defda15e81c9c7277
Kishor <kishro.r1985@gmail.com>
2022-09-07 22:27:44 +0530

    logic for search

--- src/Components/ImageList/ImageSection/index.js
+++ src/Components/ImageList/ImageSection/index.js
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 import ImageComponent from "Components/Common/Image";
 import "Styles/ImageList/index.css";
 
-const ImageSection = ({ imageList = {}, selectedCategory = "" }) => {
+const ImageSection = ({
+  imageList = {},
+  selectedCategory = "",
+  searchText = "",
+}) => {
   console.log("inside ImageSection imageList:", imageList);
   console.log("inside ImageSection selectedCategory:", selectedCategory);
   const imageArray = [];
@@ -17,30 +21,36 @@ const ImageSection = ({ imageList = {}, selectedCategory = "" }) => {
   return (
     <div className="imageFlex">
       {imageArray &&
-        imageArray.map((val, index) => {
-          const { image, text } = val || {};
-          return (
-            <div key={`${val.text}-${index}`}>
-              <ImageComponent
-                imageURL={image}
-                imageHeight="400px"
-                imageWidth="400px"
-                imageText={text}
-              />
-            </div>
-          );
-        })}
+        imageArray
+          .filter((val) =>
+            searchText ? val.text.indexOf(searchText) > -1 : true
+          )
+          .map((val, index) => {
+            const { image, text } = val || {};
+            return (
+              <div key={`${val.text}-${index}`}>
+                <ImageComponent
+                  imageURL={image}
+                  imageHeight="400px"
+                  imageWidth="400px"
+                  imageText={text}
+                />
+              </div>
+            );
+          })}
     </div>
   );
 };
 
 ImageSection.propTypes = {
   imageList: PropTypes.object,
+  searchText: PropTypes.string,
   selectedCategory: PropTypes.string,
 };
 
 ImageSection.defaultProps = {
   imageList: {},
+  searchText: "",
   selectedCategory: "",
 };
 
